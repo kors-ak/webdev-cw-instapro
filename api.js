59c806f213a765efd4d87cdf98587c55b6a6d767
kors-ak <alinakorsak86@gmail.com>
2026-04-27 17:04:40 +0300

    feat: add page for creating new posts

--- api.js
+++ api.js
@@ -1,66 +1,86 @@
 const baseHost = "https://wedev-api.sky.pro/api";
-const postsHost = `${baseHost}/v1/prod/instapro`;
+const postsHost = `${baseHost}/v1/kkk/instapro`;
 
 export function getPosts({ token }) {
-  return fetch(postsHost, {
-    method: "GET",
-    headers: {
-      Authorization: token,
-    },
-  })
-    .then((response) => {
-      if (response.status === 401) {
-        throw new Error("Нет авторизации");
-      }
+ return fetch(postsHost, {
+  method: "GET",
+  headers: {
+   Authorization: token,
+  },
+ })
+  .then((response) => {
+   if (response.status === 401) {
+    throw new Error("Нет авторизации");
+   }
 
-      return response.json();
-    })
-    .then((data) => {
-      return data.posts;
-    });
+   return response.json();
+  })
+  .then((data) => {
+   return data.posts;
+  });
+}
+
+export function addPost(token, description, imageUrl) {
+ return fetch(postsHost, {
+  method: "POST",
+  headers: {
+   Authorization: token,
+  },
+  body: JSON.stringify({
+   description,
+   imageUrl,
+  }),
+ }).then((response) => {
+  if (!response.ok) {
+   return response.json().then((errorData) => {
+    throw new Error(errorData.error || "Ошибка на сервере");
+   });
+  }
+  return response.json();
+ });
 }
 
 export function registerUser({ login, password, name, imageUrl }) {
-  return fetch(baseHost + "/user", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-      name,
-      imageUrl,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Такой пользователь уже существует");
-    }
-    return response.json();
-  });
+ return fetch(baseHost + "/user", {
+  method: "POST",
+  body: JSON.stringify({
+   login,
+   password,
+   name,
+   imageUrl,
+  }),
+ }).then((response) => {
+  if (response.status === 400) {
+   throw new Error("Такой пользователь уже существует");
+  }
+  return response.json();
+ });
 }
 
 export function loginUser({ login, password }) {
-  return fetch(baseHost + "/user/login", {
-    method: "POST",
-    body: JSON.stringify({
-      login,
-      password,
-    }),
-  }).then((response) => {
-    if (response.status === 400) {
-      throw new Error("Неверный логин или пароль");
-    }
-    return response.json();
-  });
+ return fetch(baseHost + "/user/login", {
+  method: "POST",
+  body: JSON.stringify({
+   login,
+   password,
+  }),
+ }).then((response) => {
+  if (response.status === 400) {
+   throw new Error("Неверный логин или пароль");
+  }
+  return response.json();
+ });
 }
 
 // Загружает картинку в облако, возвращает url загруженной картинки
 export function uploadImage({ file }) {
-  const data = new FormData();
-  data.append("file", file);
+ const data = new FormData();
+ data.append("file", file);
 
-  return fetch(baseHost + "/upload/image", {
-    method: "POST",
-    body: data,
-  }).then((response) => {
-    return response.json();
-  });
+ return fetch(baseHost + "/upload/image", {
+  method: "POST",
+  body: data,
+ }).then((response) => {
+  return response.json();
+ });
 }
--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,23 +1,52 @@
+import { renderHeaderComponent } from "./header-component.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
-  const render = () => {
-    // @TODO: Реализовать страницу добавления поста
-    const appHtml = `
+ const render = () => {
+  const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
+      <div class="form">
+        <h3 class="form-title">
+          Cтраница добавления поста
+        </h3>
+        <div class="form-inputs">
+          <div class="upload-image-container"></div>
+          <input type="text" id="text-input" class="input" placeholder="Введите текст" />
+          <div class="form-error"></div>
+        </div>
+      </div>
       <button class="button" id="add-button">Добавить</button>
     </div>
   `;
+  appEl.innerHTML = appHtml;
+ };
 
-    appEl.innerHTML = appHtml;
+ render();
 
-    document.getElementById("add-button").addEventListener("click", () => {
-      onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
-      });
-    });
-  };
+ renderHeaderComponent({
+  element: document.querySelector(".header-container"),
+ });
+ document.querySelector(".add-post-sign").style.visibility = "hidden";
 
-  render();
+ const uploadImageContainer = appEl.querySelector(".upload-image-container");
+ let imageUrl = "";
+
+ if (uploadImageContainer) {
+  renderUploadImageComponent({
+   element: uploadImageContainer,
+   onImageUrlChange(newImageUrl) {
+    imageUrl = newImageUrl;
+   },
+  });
+ }
+
+ document.getElementById("add-button").addEventListener("click", () => {
+  let text = document.getElementById("text-input").value;
+
+  onAddPostClick({
+   description: `${text}`,
+   imageUrl: `${imageUrl}`,
+  });
+ });
 }
